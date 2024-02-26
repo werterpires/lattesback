36fc8705cdcccbc693ef519353d2d88763c06cb8
werterpires <werterpires23@hotmail.com>
2024-02-26 18:06:23 -0300

    add errors service

--- src/app/shared/shared-services/errors-service/errors-service.service.ts
+++ src/app/shared/shared-services/errors-service/errors-service.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
-export class ErrorsServiceService {}
+export class ErrorsServiceService {
+  handleErrors(error: any, message: string, func: string): any {
+    // console.error('erro para debug:', error);
+    if (error.message.startsWith('#')) {
+      return error;
+    } else {
+      console.error(`new error in  ${func}:`);
+      console.error(error);
+      return new InternalServerErrorException(
+        'Impossível de realizar a operação',
+      );
+    }
+  }
+}
