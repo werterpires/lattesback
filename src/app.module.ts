1e12cc25d2e9316755c45fce993a825497a78c42
werterpires <werterpires23@hotmail.com>
2024-03-04 08:03:27 -0300

    add login resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ErrorsServiceService } from './app/shared/shared-services/errors-service/errors-service.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LoginModule } from './app/shared/login/login.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASECONECT)],
+  imports: [MongooseModule.forRoot(process.env.DATABASECONECT), LoginModule],
   controllers: [AppController],
   providers: [AppService, ErrorsServiceService],
 })
