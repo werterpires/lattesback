736b6e73890353f08814c0b0e2ed649197d2e9be
werterpires <werterpires23@hotmail.com>
2024-04-04 17:18:16 -0300

    add port to port 3001

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -11,6 +13,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(3001);
+  app.listen(process.env['PORT'] || 3001);
 }
 bootstrap();
